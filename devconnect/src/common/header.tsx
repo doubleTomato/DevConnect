f3f1909f1cc0117589c4f599b00a9ff239e321c4
doubleTomato <doubletomato21@gmail.com>
2025-02-10 21:26:55 +0900

    [add] home레이아웃 추가 및 로고 추가

--- devconnect/src/common/header.tsx
+++ devconnect/src/common/header.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-    const headerItems:string[] = ['Home', 'Blog', 'Projects', 'Profile'];
+    const headerItems:string[] = ['', 'Blog', 'Projects', 'Profile'];
     const headerLink:string[] = ['/', '/blog', '/projects', '/profile'];
     return (
-        <ul className="flex gap-2 bg-gray-800 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            {headerItems.map((v,i) => <Link to={headerLink[i]}><li className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset" key={i}>{v}</li></Link>)}
-        </ul>
+        <div className="flex mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div>Logo</div>
+            <ul className="flex gap-2 bg-gray-800 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                {headerItems.map((v,i) => <Link to={headerLink[i]}><li className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset" key={i}>{v}</li></Link>)}
+            </ul>
+        </div>
+
     )
 }
 
--- devconnect/src/pages/Home.tsx
+++ devconnect/src/pages/Home.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Home(){
-    return "Home";
+    return (
+    <section className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        {/* filter자리 */}
+        <nav>
+            <ul></ul>
+        </nav>
+        <article>main</article>
+        <article>blog</article>
+    </section>);
 }
 
 export default Home;
