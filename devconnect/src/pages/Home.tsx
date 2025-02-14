c77d04a2a4c016d96c9c11f243c5128cbc174b70
doubleTomato <doubletomato21@gmail.com>
2025-02-14 22:08:28 +0900

    [add] Home

--- devconnect/src/pages/Home.tsx
+++ devconnect/src/pages/Home.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 
 function Home(){
+    const filterList = ['abc','def','fes','fewf'];
     return (
-    <section className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <section className="mx-auto max-w-7xl">
+        <article className="flex h-64 bg-blue-500 mx-auto">
+            <div>인기</div>
+            <div>
+                <button>내블로그</button>
+                <button>글쓰기</button>
+            </div>
+        </article>
         {/* filter자리 */}
-        <nav>
-            <ul></ul>
+        <nav >
+            <ul className="flex ">
+                {filterList.map((x,i) => <li className="rounded-xl px-4 py-1 bg-red-500 mr-2 my-4" key={i}>#{x}</li>)}
+            </ul>
         </nav>
-        <article>main</article>
-        <article>blog</article>
+        <h2>Blog</h2>
+        <article>
+            
+        </article>
+        <h2>Projects</h2>
+        <article>
+            
+        </article>
     </section>);
 }
 
